e960e1c1fa1e8ee455057585d5561266b3562115
Ikechukwu <iyke497@gmail.com>
2022-09-10 08:54:28 +0000

    print u and l

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	/*Print lower case chars*/
 	int lc = 97;
+	int uc = 65;
 
 	do {
 		putchar(lc);
@@ -16,8 +17,6 @@ int main(void)
 	} while (lc < 123);
 
 	/*Print upper case chars*/
-	int uc = 65;
-
 	do {
 		putchar(uc);
 		if (uc == 90)
