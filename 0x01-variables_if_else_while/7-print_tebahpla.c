63e590cfc7e8f1f486fbd1ac72f31d04bbb6b374
Ikechukwu <iyke497@gmail.com>
2022-09-10 23:46:50 +0000

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int c = 122;
+
+	do {
+		putchar(c);
+		if (c == 97)
+			putchar('\n');
+		c--;
+	} while (c > 96);
+}
