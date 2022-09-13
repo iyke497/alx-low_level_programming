70746107e2831654919a9569dbadd518eb48c83f
Ikechukwu <iyke497@gmail.com>
2022-09-13 15:43:21 +0000

    98 streert

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,21 @@
  */
 void print_to_98(int n)
 {
-	if (n > '98')
+	if (n > 98)
 	{
 		do {
 			_putchar(n);
 			n--;
 		} while (n >= 98);
 	}
-	else if (n == '98')
+	else if (n == 98)
 	{
 		_putchar(n);
 	}
 	else
 	{
 		do {
-			_putchar("n", % u);
+			_putchar(n);
 			n++;
 		} while (n <= 98)
 	}
