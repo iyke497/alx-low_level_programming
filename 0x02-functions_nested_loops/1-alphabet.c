8d4380a6e42729d0f9a9967720317e8a0c2e2b67
Ikechukwu <iyke497@gmail.com>
2022-09-13 12:15:35 +0000

    Alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,3 +10,20 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+/**
+ * print_alphabet - Prints alphabet in lowercase followed by new line.
+ *
+ * Return: Success (0)
+ */
+int print_alphabet(void)
+{
+	int lc = 97;
+
+	do {
+		_putchar(lc);
+		if (lc == 122)
+			_putchar('\n');
+		lc++;
+	} while (lc < 123);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,9 @@
 #include <unistd.h>
 
-/**
- ** _putchar - writes the character c to stdout
- ** @c: The character to print
- **
- ** Return: On success 1.
- ** On error, -1 is returned, and errno is set appropriately.
- **/
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
-int print_alphabet(void)
-{
-	int lc = 97;
+int print_alphabet(void);
 
-	do {
-		_putchar(lc);
-		if (lc == 122)
-			_putchar('\n');
-		lc++;
-	} while (lc < 123);
-
-	return (0);
-}
