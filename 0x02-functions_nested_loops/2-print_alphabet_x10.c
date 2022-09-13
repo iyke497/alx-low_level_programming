1927a1ca01794756c7950c80468a2ec66ae29049
Ikechukwu <iyke497@gmail.com>
2022-09-13 13:15:42 +0000

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+int print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - Prints the aalphabet ten times to stdout
+ *
+ * Return: Success (0)
+ */
+int print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+
+}
+/**
+ * print_alphabet - prints alhabet to stdout
+ *
+ * Return: Succes (0)
+ */
+int print_alphabet(void)
+{
+	int lc = 97;
+
+	do {
+		_putchar(lc);
+		if (lc == 122)
+			_putchar('\n');
+		lc++;
+	} while (lc < 123);
+
+	return (0);
+}
