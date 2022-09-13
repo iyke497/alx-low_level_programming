04263cb5c50187c8e06f75f8f62c4d79b4c8c032
Ikechukwu <iyke497@gmail.com>
2022-09-13 10:55:09 +0000

    0._PUTCHAR

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	/*Initialize the string*/
+	char lib[9] = "_putchar";
+
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(lib[i]);
+		if (i == 8)
+			_putchar('\n');
+		i++;
+	}
+
+	return (0);
+}
