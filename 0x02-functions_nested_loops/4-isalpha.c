d7d5be0b6881ef7cb22f586b9dde46bfde7fb51e
Ikechukwu <iyke497@gmail.com>
2022-09-13 13:50:08 +0000

    Check for alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is upper or lowercase
+ * @c: Character to be checked
+ *
+ * Return: 1 for upper/lowercase letter 0 otherwise
+ */
+int _isalpha(int c)
+{
+	int c;
+
+	/*Lowercase check*/
+	if (c > 64 && c < 123)
+	{
+		if (c > 90 && c < 97)
+			return (0);
+		else
+			return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
