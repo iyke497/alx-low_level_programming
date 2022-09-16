98df1b35b53a95e8e60b629fdecc10285c3ab79b
Ikechukwu <iyke497@gmail.com>
2022-09-16 08:42:49 +0000

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line to stdout
+ * @n: Number of times to print line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+}	
